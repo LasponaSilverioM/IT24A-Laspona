46997037bd4fc064d7d0664be9ab1ea458c4bd46
LasponaSilverioM <tawinlaspona@gmail.com>
2024-10-01 10:34:46 +0800

    added constructor

--- applet-2.js
+++ applet-2.js
@@ -3,4 +3,12 @@ class leafletmap{
    this.map = L.map.setView(center, zoom);
     this.initTileLayer();
 }
+initTileLayer(){
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        
+    }).addTo(map);
+}
 }
