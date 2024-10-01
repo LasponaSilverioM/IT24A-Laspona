658469b3daa69bc613b9569fd3cdc0ed794a886b
Louie Jay Laspoña <tawinlaspona@gmail.com>
2024-10-01 21:45:05 +0800

    Added  loadMarkersFromJson

--- applet-2.js
+++ applet-2.js
@@ -13,8 +13,17 @@ class LeafletMap {
         const marker = L.marker([lat, lng]).addTo(this.map);
         marker.bindPopup(message);
     }
-
-
+    loadMarkersFromJson(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(marker => {
+                    this.addMarker(marker.latitude, marker.longitude, marker.message);
+                });
+            })
+            .catch(error => console.error('Error loading markers:', error));
+    }
+}
     const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
 
     
