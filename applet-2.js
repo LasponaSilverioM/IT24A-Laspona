9511a427f753390e47f7ba1ad539afaa6452dfe6
Louie Jay Laspoña <tawinlaspona@gmail.com>
2024-10-06 17:03:43 +0800

    Modified object

--- applet-2.js
+++ applet-2.js
@@ -114,7 +114,7 @@ updateLogDisplay() {
 
 }
 
-const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
+const Mymap = new LeafletMap('map', [8.359735, 124.869206], 18);
 
 myMap.addMarker(8.359735, 124.869206, 'CCS Faculty Office');
 myMap.addMarker(8.359639, 124.869179, 'CCS Laboratory 1');
