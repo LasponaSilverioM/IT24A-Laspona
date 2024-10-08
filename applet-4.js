89e75ac26629435ede8ade70bb047ed7be5b7882
LasponaSilverioM <tawinlaspona@gmail.com>
2024-10-08 15:34:54 +0800

    Added updateMarkerPopup

--- applet-4.js
+++ applet-4.js
@@ -50,6 +50,10 @@ class LeafletMap {
 
         this.markers.push(marker);
     }
+    updateMarkerPopup(marker, message) {
+        const count = this.markerCounts[message];
+        marker.bindPopup(`${message}<br>Attendance logs: ${count}`).openPopup();
+    }
 
 
 
