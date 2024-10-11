b94406adb36de8aedf7746d5c313e591cdbfcd04
LasponaSilverioM <tawinlaspona@gmail>
2024-10-11 18:02:21 +0800

    Added init

--- applet-5.js
+++ applet-5.js
@@ -5,6 +5,11 @@ class StudentList {
         this.students = [];
         this.init();
     }
+    async init() {
+        await this.fetchData();
+        this.renderStudentList(this.students); 
+        this.bindSearchEvent();
+    }
 
 
 
