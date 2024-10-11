77646d1e41b0fa8233077c6b9cae008f3fdb4c5b
LasponaSilverioM <tawinlaspona@gmail.com>
2024-10-11 11:46:22 +0800

    Added filterStudents

--- applet-5.js
+++ applet-5.js
@@ -37,15 +37,12 @@ class StudentList {
 
         this.renderStudentList(this.students, studentSearchListContainer);
     }
-
-
-
-
-
-
-
-
-
+    filterStudents(query, searchListContainer) {
+        const filteredStudents = this.students.filter(student => {
+            const fullName = `${student.student_name} ${student.student_program}`;
+            return fullName.toLowerCase().includes(query.toLowerCase());
+        });
+    }
 
 
 
