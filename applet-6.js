02d8a8e1b2567f05930dd044582ca69e5c3d95fa
LasponSilverioM <tawinlaspona@gmail.com>
2024-10-25 14:10:32 +0800

    Added TimestampedTodoList

--- applet-6.js
+++ applet-6.js
@@ -58,6 +58,18 @@ class TodoList {
         this.editingIndex = -1;
         this.addButton.textContent = 'Add';
     }
-     
+
 }
+
+class TimestampedTodoList extends TodoList {
+    addTask(taskText) {
+        super.addTask(taskText);
+        const taskItem = this.todoList.lastChild; // Get the newly added task
+        const timestamp = document.createElement('span');
+        timestamp.className = 'timestamp';
+        timestamp.textContent = new Date().toLocaleString();
+        taskItem.appendChild(timestamp);
+    }
+}
+
 
